986cdad6b1ebe28d2a412714a64477b9dfcafc5d
Iury <iurysena74@gmail.com>
2024-09-07 09:57:58 -0300

    feat: finish home page's hero

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,8 +5,8 @@ import { HeroCarousel } from "./hero-carousel";
 export default function Home() {
   return (
     <div>
-      <section className="flex items-center justify-between px-32 py-16 h-[28rem] bg-no-repeat bg-cover bg-slate-950/30">
-        <div className="space-y-11">
+      <section className="relative flex items-center justify-between px-32 py-16 w-full h-[28.9375rem] bg-[url('/assets/hero.jpg')] bg-no-repeat bg-cover bg-slate-950/30">
+        <div className="space-y-11 z-[1] self-start">
           <div className="flex flex-col">
             <span className="regular-2xl">
               Olá, <b>Faça seu login!</b>
@@ -21,10 +21,12 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 z-[1]">
           <span className="bold-sm text-gray-300">RECOMENDADOS</span>
           <HeroCarousel />
         </div>
+
+        <div className="w-full h-full left-0 absolute bg-black/85 z-0" />
       </section>
     </div>
   );
